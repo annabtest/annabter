d7a2d33dc15ae43c179919eb6a37d754e7c81f9f
Anna <boolanna@gmail.com>
2025-01-11 22:17:08 +0200

    again

--- variable.tf
+++ variable.tf
@@ -3,11 +3,6 @@ variable "client_id" {
   default = "a3827f98-17f7-4d7d-9d12-fd70b34c65fe"
 }
 
-# variable "client_secret" {
-#   type = string
-#   default = "Bps8Q~WM.QgY~hZ9JZ~lBMMdT~lopP3htbJN1bOR"
-# }
-
 variable "subscription_id" {
   type = string
   default = "80b6af06-ee48-49d6-9a29-31835d24d625"
