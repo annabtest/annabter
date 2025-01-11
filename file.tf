6f1a820034fa6929bbf3c379a07d1d648a9a4cdb
Anna <boolanna@gmail.com>
2025-01-11 22:20:19 +0200

    no auth

--- file.tf
+++ file.tf
@@ -10,10 +10,6 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  tenant_id = var.tenant_id
-
   features {}
 
   skip_provider_registration = "true"
