7207866065dd0b2028b2be00a24ebef167ad7daf
Anna <boolanna@gmail.com>
2025-01-13 11:50:45 +0200

    with pip

--- main.tf
+++ main.tf
@@ -2,5 +2,13 @@ module "resource_group" {
     source = "./modules/resource_group"
     
     rg_location = var.location
-    naming_suffix = local.naming_prefix
+    naming_suffix = local.naming_suffix
+}
+
+module "public_ips" {
+    source "./modules/public_ips"
+
+    pip_location = var.location
+    naming_suffix = local.naming_suffix
+    rg_name = module.resource_group.rg_NAME
 }
