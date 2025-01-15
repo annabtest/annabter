d8968c5d8aa01c163e0050f1e0c52fa6f1396bb5
Anna <boolanna@gmail.com>
2025-01-15 17:26:36 +0200

    typo

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "hub_network" {
   source = "./modules/network"
   
   location = var.location
-  rg_name = module.resource_group.rg_NAME
+  rg_name = module.hub_resource_group.rg_NAME
   vnet_name = "hub_vnet_${local.naming_suffix}"
   naming_suffix = local.naming_suffix
   vnet_address_space = var.hub_vnet_address_space
