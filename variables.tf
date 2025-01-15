be7cc049ef1cde15458e6e4a8f540447d3f75a08
Anna <boolanna@gmail.com>
2025-01-15 20:44:55 +0200

    vars

--- variables.tf
+++ variables.tf
@@ -17,3 +17,36 @@ variable "tenant_id" {
   type    = string
   default = "dc00758b-d159-4ec1-965d-31eafd5a603e"
 }
+
+# Naming Convention Values
+variable "region_code" {
+  type = string
+}
+
+variable "envioronment" {
+  type = string
+}
+
+variable "department" {
+  type = string
+}
+
+# General Properties
+variable "location" {
+  type = string
+}
+
+
+
+# Network Configuration
+variable "hub_vnet_address_space" {
+  type = list(string)
+}
+
+variable "azfw_subnet_address_prefixes" {
+  type = list(string)
+}
+
+variable "azfw_subnet_name" {
+  type = string
+}
