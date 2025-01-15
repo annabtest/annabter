41d0196e69efecc8653f35baa5d9c828cc1e54f3
Anna <boolanna@gmail.com>
2025-01-15 20:42:51 +0200

    format

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "client_id" {
-  type = string
+  type    = string
   default = "a3827f98-17f7-4d7d-9d12-fd70b34c65fe"
 }
 
 variable "client_secret" {
-  type = string
+  type    = string
   default = "Bps8Q~WM.QgY~hZ9JZ~lBMMdT~lopP3htbJN1bOR"
 }
 
 variable "subscription_id" {
-  type = string
+  type    = string
   default = "80b6af06-ee48-49d6-9a29-31835d24d625"
 }
 
 variable "tenant_id" {
-  type = string
+  type    = string
   default = "dc00758b-d159-4ec1-965d-31eafd5a603e"
 }
