86bf576ee6ff16904d7187a5aa675f84a3adb36c
Anna <boolanna@gmail.com>
2025-01-29 22:33:47 +0200

    dns rec

---
+++ modules/dns_record/variables.tf
@@ -0,0 +1,11 @@
+variable "zone_name" {
+  type = string
+}
+
+variable "record_name" {
+  type = string
+}
+
+variable "public_ip" {
+  type = list(string)
+}
--- modules/public_ips/main.tf
+++ modules/public_ips/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "public_ip" {
-  name                = "${var.naming_prefix}_${var.naming_suffix}"
+  name                = "${var.naming_prefix}-${var.naming_suffix}"
   resource_group_name = var.rg_name
   location            = var.pip_location
   allocation_method   = "Static"
