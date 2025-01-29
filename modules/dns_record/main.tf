82e0d400d0069f3ef0e3dc603021f13dc8b33e6b
Anna <boolanna@gmail.com>
2025-01-29 22:57:06 +0200

    dns

--- modules/dns_record/main.tf
+++ modules/dns_record/main.tf
@@ -1,9 +1,3 @@
-resource "dns_a_record_set" "record" {
-  zone      = var.zone_name
-  name      = var.record_name
-  addresses = var.public_ip
-}
-
 resource "azurerm_dns_a_record" "record" {
   name                = var.record_name
   resource_group_name = var.resource_group
