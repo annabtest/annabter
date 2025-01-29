35f872022864ac15ad530d7040a89c52bb83498e
Anna <boolanna@gmail.com>
2025-01-29 21:40:49 +0200

    aks and pool

---
+++ modules/aks_linux_pool/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_kubernetes_cluster_node_pool" "linux_pool" {
+  zones                 = [1]
+  enable_auto_scaling   = true
+  kubernetes_cluster_id = var.kubernetes_cluster_id
+  max_count             = 1
+  min_count             = 1
+  mode                  = "User"
+  name                  = "linux-pool-${var.name_suffix}"
+  orchestrator_version  = var.kubernetes_version
+  os_type               = var.os_type
+  vm_size               = "Standard_DS2_v2"
+}
