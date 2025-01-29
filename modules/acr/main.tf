e35b9876b582af339256b0c28a82ba6c59f69821
Anna <boolanna@gmail.com>
2025-01-29 20:45:36 +0200

    name

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "acr${var.name_suffix}"
+  name                = "acr${var.name_suffix}8"
   resource_group_name = var.resource_group
   location            = var.location
   sku                 = "Basic"
