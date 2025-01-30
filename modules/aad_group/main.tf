0a91db7a409218ba4cba14a70ed94f88051f823a
Anna <boolanna@gmail.com>
2025-01-30 15:10:55 +0200

    role

--- modules/aad_group/main.tf
+++ modules/aad_group/main.tf
@@ -1,3 +1,6 @@
+data "azurerm_subscriptions" "available" {
+    display_name_contains = var.ARM_SUBSCRIPTION_ID
+}
 resource "azuread_group" "example" {
   display_name     = var.group_name
   security_enabled = true
@@ -6,7 +9,7 @@ resource "azuread_group" "example" {
 }
 
 resource "azurerm_role_assignment" "example" {
-  scope                = var.ARM_SUBSCRIPTION_ID
+  scope                = data.azurerm_subscriptions.available.id
   role_definition_name = "ArcPull"
   principal_id         = azuread_group.example.id
 }
