3e5d809b35e12db86be7f38c273473848bd21194
Anna <boolanna@gmail.com>
2025-01-30 19:53:12 +0200

    auto scale

--- modules/aks_node_pool/main.tf
+++ modules/aks_node_pool/main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_kubernetes_cluster_node_pool" "node_pool" {
   zones                 = [3]
   kubernetes_cluster_id = var.kubernetes_cluster_id
-  max_count             = null
-  min_count             = null
+  max_count             = 1
+  min_count             = 1
   mode                  = "User"
   name                  = "${var.os_type}${var.name_suffix}"
   orchestrator_version  = var.kubernetes_version
   os_type               = var.Os_type
   vm_size               = "Standard_DS2_v2"
-  enable_auto_scaling   = true
+  auto_scaling_enabled   = true
   upgrade_settings {
     max_surge = "10%"
   }
